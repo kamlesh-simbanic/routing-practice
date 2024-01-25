94986344cdeed562883ee245e2511509f08de491
Kamlesh Thavani <kamlesh.simbanic2022@gmail.com>
2024-01-25 15:45:44 +0530

    Nested Dynamic Routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,16 @@
+export default function ProductDetail({
+  params,
+}: {
+  params: {
+    productId: string;
+    reviewId: string;
+  };
+}) {
+  return (
+    <>
+      <h1>
+        Review {params.reviewId} for Product {params.productId}
+      </h1>
+    </>
+  );
+}
